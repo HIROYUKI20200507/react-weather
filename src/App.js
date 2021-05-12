1f2d98f3dd9c61d329a1f2e31baed6f09b74a8aa
hiroyuki.akimoto <hiroyuki.akimoto@willof.co.jp>
2021-05-12 22:17:30 +0900

    東京常時表示＋検索ボタン追加

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import axios from 'axios';
 import SearchBar from './components/SearchBar';
 
 const App = () => {
+    
+    window.onload = async () => {
+        const def = document.getElementById('default');
+        const new_def = document.createElement('h2');
+        
+        const params = {
+            key: "bd516a21b339b45198db52801af10c11",
+            q: "東京",
+        };
+        const response = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + ( params.q ) + "&APPID=" + ( params.key ));
+        
+        def.textContent = ( response.data.weather[0].main );
+        def.appendChild(new_def);
+    };
+
     const onSearchSubmit = async (term) => {
         try {
+            const stage = document.getElementById('stage');
+            const def = document.getElementById('default');
+            const new_stage = document.createElement('h1');
+            const new_def = document.createElement('h2');
+    
             const params = {
                 key: "bd516a21b339b45198db52801af10c11",
                 q: term,
             };
             const response = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + ( params.q ) + "&APPID=" + ( params.key ));
-            alert('今日の天気は' + ( response.data.weather[0].main ) + 'です。');
+
+            stage.textContent = ( term );
+            def.textContent = ( response.data.weather[0].main );
+            stage.appendChild(new_stage);
+            def.appendChild(new_def);
+    
         } catch {}
     };
     return (
         <div className='ui container' style={{marginTop:'20px'}}>
             <SearchBar onSubmit={onSearchSubmit}/>
+            <div>
+                <h1 id="stage">東京</h1>
+                <h1 id="default"></h1>
+            </div>
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,18 +8,22 @@ const SearchBar = ({ onSubmit }) => {
     };
     return (
         <form onSubmit={onFormSubmit} className="ui form">
-        <div className="field">
-            <label>Weather Search</label>
-            <input 
-                type="text" 
-                name="search" 
-                placeholder="都道府県を入力してください" 
-                value={term}
-                onChange={(event) => {
-                    setTerm(event.target.value);
-                }}
-            />
-        </div>
+            <div className="field">
+                <label>Weather Search</label>
+                <input 
+                    type="text" 
+                    name="search" 
+                    placeholder="都道府県を入力してください" 
+                    value={term}
+                    onChange={(event) => {
+                        setTerm(event.target.value);
+                    }}
+                />
+            </div>
+            <button class="ui primary button">検索</button>
+            <div>
+                
+            </div>
         </form>
     );
 };
