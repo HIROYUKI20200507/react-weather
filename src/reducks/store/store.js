c4c6c43259c4b0a5254086a61f66e094758963bc
Hiroyuki <dokomademo1124@gmail.com>
2021-05-21 09:38:45 +0900

    [modi]コメント追加

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,13 +1,13 @@
 import {
     createStore as reduxCreateStore,
-    combineReducers
+    combineReducers,
 } from 'redux';
-
 // import { productsReducer } from "../products/reducers";
 import { UsersReducer } from "../users/reducers";
 
 export default function createStore() {
     return reduxCreateStore( //reduxのcreateStoreメソッドの別名
+        // combineReducersでオブジェクトガッチャンコ
         combineReducers({
             // products: productsReducer,
             users: UsersReducer,
