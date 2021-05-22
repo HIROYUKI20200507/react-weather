ddb7ab1b24b5112d641ee45c98658d9c3106adad
Hiroyuki <dokomademo1124@gmail.com>
2021-05-22 23:59:43 +0900

    [modi]redux react-redux install react Hooks処理記述

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "./reducks/store/store";
+import createStore from "./reducks/store/store";
 // import * as serviceWorler from "./serviceWorler";
 import App from './App';
 
