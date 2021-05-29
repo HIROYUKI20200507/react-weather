4b3e00a363472da017c4185a9cd29841e73fc907
Hiroyuki <dokomademo1124@gmail.com>
2021-05-29 23:39:56 +0900

    [modi]sagaとstoreの接続確認※無限ループエラーあり

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -4,17 +4,17 @@ import {
 } from 'redux';
 // import { productsReducer } from "../products/reducers";
 import { UsersReducer } from "../users/reducers";
-import { createSagaMiddleware } from "redux-saga";
-import { rootSaga } from "src/sagas";
-
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-    reducers,
-    applyMiddleware(sagaMiddleware)
-)
-sagaMiddleware.run(rootSaga);
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "../../sagas/index";
 
 export default function createStore() {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = createStore(
+        UsersReducer,
+        applyMiddleware(sagaMiddleware)
+    )
+    sagaMiddleware.run(rootSaga);
+
     return reduxCreateStore( //reduxのcreateStoreメソッドの別名
         // combineReducersでオブジェクトガッチャンコ
         combineReducers({
